41d57f277e6077b4bdb6b272e4dd24135b7ce7b5
Deyon Avoseh <avodeyon.07@gmail.com>
2026-02-21 23:02:57 +0100

    fix: use explicit django.views.generic.detail import for DetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
